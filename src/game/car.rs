45c181096c712630bdfd8824d1470559fd054261
Icey1717 <icey1717@gmail.com>
2022-12-30 09:43:31 +1100

    Steering with velocity.

--- src/game/car.rs
+++ src/game/car.rs
@@ -17,7 +17,7 @@ enum PlayerLoadState {
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<CarSpriteHandles>()
-        .init_resource::<PlayerDebugResource>()
+            .init_resource::<PlayerDebugResource>()
             .add_state(PlayerLoadState::Setup)
             .add_system_set(SystemSet::on_enter(PlayerLoadState::Setup).with_system(load_textures))
             .add_system_set(
@@ -26,7 +26,12 @@ impl Plugin for PlayerPlugin {
             .add_system_set(SystemSet::on_enter(PlayerLoadState::Finished).with_system(setup))
             .add_system_set(SystemSet::on_enter(game::AppState::Loaded).with_system(setup_debug))
             .add_system_set(SystemSet::on_update(PlayerLoadState::Finished).with_system(tick))
-            .add_system_set(SystemSet::on_update(PlayerLoadState::Finished).with_system(update_debug));
+            .add_system_set(
+                SystemSet::on_update(PlayerLoadState::Finished).with_system(update_debug),
+            )
+            .add_system_set(
+                SystemSet::on_update(PlayerLoadState::Finished).with_system(update_debug_alt),
+            );
     }
 }
 
@@ -72,7 +77,29 @@ fn setup_debug(
     for player in debug.player_entities.iter() {
         log::log!("Setting up debug for player: {:?}", player);
         commands.spawn(PlayerDebugBundle {
-            text: Text2dBundle {
+            text: Text2dBundle { ..default() },
+            debug: PlayerDebug { id: *player },
+        });
+
+        commands.spawn(PlayerDebugSpriteBundle {
+            sprite: SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(0.25, 0.25, 0.75),
+                    custom_size: Some(Vec2::new(10.0, 10.0)),
+                    ..default()
+                },
+                ..default()
+            },
+            debug: PlayerDebug { id: *player },
+        });
+
+        commands.spawn(PlayerDebugSpriteBundle {
+            sprite: SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(0.25, 0.25, 0.75),
+                    custom_size: Some(Vec2::new(10.0, 10.0)),
+                    ..default()
+                },
                 ..default()
             },
             debug: PlayerDebug { id: *player },
@@ -97,28 +124,31 @@ fn setup(
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
     // draw a sprite from the atlas
-    debug.player_entities.push(commands
-        .spawn(PlayerCarBundle {
-            sprite: SpriteSheetBundle {
-                transform: Transform {
-                    translation: Vec3::new(450.0, 250.0, 1.0),
-                    scale: Vec3::splat(1.0),
+    debug.player_entities.push(
+        commands
+            .spawn(PlayerCarBundle {
+                sprite: SpriteSheetBundle {
+                    transform: Transform {
+                        translation: Vec3::new(450.0, 250.0, 1.0),
+                        scale: Vec3::splat(1.0),
+                        ..default()
+                    },
+                    sprite: TextureAtlasSprite::new(0),
+                    texture_atlas: texture_atlas_handle,
                     ..default()
                 },
-                sprite: TextureAtlasSprite::new(0),
-                texture_atlas: texture_atlas_handle,
+                player_car: PlayerCar {
+                    velocity: Vec2 { x: 0.0, y: 0.0 },
+                    acceleration: Vec2 { x: 0.0, y: 0.0 },
+                    max_speed: 100.0,
+                    size: CAR_SIZE_PX,
+                    heading: Vec2 { x: 1.0, y: 0.0 },
+                    ..default()
+                },
                 ..default()
-            },
-            player_car: PlayerCar {
-                velocity: Vec2 { x: 0.0, y: 0.0 },
-                acceleration: Vec2 { x: 0.0, y: 0.0 },
-                max_speed: 100.0,
-                size: CAR_SIZE_PX,
-                ..default()
-            },
-            ..default()
-        })
-        .id());
+            })
+            .id(),
+    );
 }
 
 #[derive(Component, Default)]
@@ -134,10 +164,53 @@ struct PlayerCar {
     velocity: Vec2,
     acceleration: Vec2,
     max_speed: f32,
-    heading: f32,
     size: Vec2,
-    wheel_base: f32,
-    steer_angle: f32,
+    front_wheel: Vec2,
+    back_wheel: Vec2,
+    heading: Vec2,
+}
+
+#[derive(Component, Default)]
+struct PlayerInput {
+    throttle: f32,
+    steering: f32,
+}
+
+fn rotate_vector(vector: Vec2, angle: f32) -> Vec2 {
+    let (sin, cos) = angle.sin_cos();
+    Vec2::new(
+        cos * vector.x - sin * vector.y,
+        sin * vector.x + cos * vector.y,
+    )
+}
+
+const WHEEL_BASE: f32 = 50.0;
+const HALF_WHEEL_BASE: f32 = WHEEL_BASE / 2.0;
+
+const MAX_STEERING: f32 = std::f32::consts::PI / 2.0;
+
+impl PlayerCar {
+    fn update_steering(&mut self, input: PlayerInput, delta_time: f32, position_2d: Vec2) {
+        // Work out where the front and back wheels will be.
+        self.back_wheel = position_2d - (self.heading * HALF_WHEEL_BASE);
+        self.front_wheel = position_2d + (self.heading * HALF_WHEEL_BASE);
+
+        self.back_wheel += self.velocity * delta_time;
+
+        if input.steering.abs() > 0.0 {
+            let steer_angle = MAX_STEERING * input.steering;
+            self.front_wheel += rotate_vector(self.velocity, steer_angle) * delta_time;
+        } else {
+            self.front_wheel += self.velocity * delta_time;
+        }
+
+        // Update the forward and velocity.
+        self.heading = (self.front_wheel - self.back_wheel).normalize();
+    }
+
+    fn get_rotatation_rads(&self) -> f32 {
+        Vec2::new(1.0, 0.0).angle_between(self.heading)
+    }
 }
 
 #[derive(Bundle)]
@@ -146,35 +219,96 @@ struct PlayerDebugBundle {
     debug: PlayerDebug,
 }
 
+#[derive(Bundle)]
+struct PlayerDebugSpriteBundle {
+    #[bundle]
+    sprite: SpriteBundle,
+    debug: PlayerDebug,
+}
+
 #[derive(Bundle, Default)]
 pub struct PlayerCarBundle {
     player_car: PlayerCar,
     #[bundle]
     sprite: SpriteSheetBundle,
+    input: PlayerInput,
 }
 
 fn update_debug(
     mut player_query: Query<(&PlayerCar, &Transform)>,
     mut player_text_query: Query<(&PlayerDebug, &mut Text, &mut Transform, Without<PlayerCar>)>,
-    debug : Res<PlayerDebugResource>,
+    debug: Res<PlayerDebugResource>,
 ) {
     for (player_debug, mut text, mut text_transform, ()) in player_text_query.iter_mut() {
         if let Ok((car, car_transform)) = player_query.get_mut(player_debug.id) {
             // do something with the components
-            *text = Text::from_sections([
-                TextSection::new(
-                    format!("v: x: {:.1}, y: {:.1}\na: x: {:.1}, y: {:.1}\ns: {:.1}\np: x: {:.1}, y: {:.1}", 
-                    car.velocity.x, car.velocity.y, 
-                    car.acceleration.x, car.acceleration.y,
-                    car.heading,
-                    car_transform.translation.x, car_transform.translation.y),
-                    debug.style.clone(),
+            *text = Text::from_sections([TextSection::new(
+                format!(
+                    "v: x: {:.1}, y: {:.1}\na: x: {:.1}, y: {:.1}\np: x: {:.1}, y: {:.1}",
+                    car.velocity.x,
+                    car.velocity.y,
+                    car.acceleration.x,
+                    car.acceleration.y,
+                    car_transform.translation.x,
+                    car_transform.translation.y
                 ),
-            ]).with_alignment(TextAlignment::CENTER);
-            *text_transform = *car_transform;
+                debug.style.clone(),
+            )])
+            .with_alignment(TextAlignment::CENTER);
+            text_transform.translation = car_transform.translation;
             text_transform.translation.z += 1.0;
         }
     }
+}
+
+fn update_debug_alt(
+    mut player_query: Query<(&PlayerCar, &Transform)>,
+    mut player_sprite_query: Query<(
+        &PlayerDebug,
+        &mut Sprite,
+        &mut Transform,
+        Without<PlayerCar>,
+    )>,
+    debug: Res<PlayerDebugResource>,
+) {
+    let mut must_be_front = false;
+    for (player_debug, mut text, mut text_transform, ()) in player_sprite_query.iter_mut() {
+        if let Ok((car, car_transform)) = player_query.get_mut(player_debug.id) {
+            if must_be_front {
+                text_transform.translation.x = car.front_wheel.x;
+                text_transform.translation.y = car.front_wheel.y;
+            } else {
+                text_transform.translation.x = car.back_wheel.x;
+                text_transform.translation.y = car.back_wheel.y;
+            }
+
+            text_transform.translation.z = car_transform.translation.z + 1.0;
+            must_be_front = true;
+        }
+    }
+}
+
+fn get_keyboard_input(keyboard_input: &Res<Input<KeyCode>>) -> PlayerInput {
+    let mut input: PlayerInput = PlayerInput {
+        throttle: 0.0,
+        steering: 0.0,
+    };
+
+    // Update the acceleration based on the keys that are currently pressed
+    if keyboard_input.pressed(KeyCode::Up) {
+        input.throttle = 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::Down) {
+        //input.throttle = -1.0;
+    }
+    if keyboard_input.pressed(KeyCode::Left) {
+        input.steering = 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::Right) {
+        input.steering = -1.0;
+    }
+
+    return input;
 }
 
 fn tick(
@@ -185,89 +319,20 @@ fn tick(
 ) {
     let delta_time = time.delta_seconds();
     for (mut car, mut transform) in player_query.iter_mut() {
-        let mut a = car.acceleration;
-        car.steer_angle = 0.0;
-        // Update the acceleration based on the keys that are currently pressed
-        if keyboard_input.pressed(KeyCode::Up) {
-            a.x += 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::Down) {
-            a.x -= 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::Left) {
-            car.steer_angle = -0.1;
-        }
-        if keyboard_input.pressed(KeyCode::Right) {
-            car.steer_angle = 0.1;
-        }
-
-        //// Normalize the acceleration so that the car doesn't move faster diagonally
-        //let b = a.try_normalize();
-        //if !b.is_none() {
-        //    a = b.unwrap();
-        //}
-
-        //// Rotate the acceleration vector by the steering angle
-
-        car.acceleration = a;
-
-        //// Update the velocity based on the acceleration and delta time
-        car.velocity += a * delta_time;
-
-        //// Clamp the velocity to the max speed
-        //let speed = car.velocity.length();
-        //if speed > car.max_speed {
-        //    car.velocity = car.velocity * (car.max_speed / speed);
-        //}
-
-        //// Save the old position so we can restore it if there's a collision
-        //let old_position = transform.translation;
-
-        //// Update the position based on the velocity and delta time
-        //transform.translation.x += car.velocity.x * delta_time;
-        //transform.translation.y += car.velocity.y * delta_time;
-
+        let input = get_keyboard_input(&keyboard_input);
         let position_2d = transform.translation.xy();
 
-        let car_angle: Vec2 = vec2(car.heading.cos() , car.heading.sin());
-        let car_steer: Vec2 = vec2((car.heading + car.steer_angle).cos() , (car.heading + car.steer_angle).sin());
+        // Calculate our new velocity, in the direction of our forward.
+        car.velocity = car.heading * (input.throttle * 50.0);
 
-        let mut front_wheel = position_2d + car.wheel_base/2.0 * car_angle;
-        let mut back_wheel = position_2d - car.wheel_base/2.0 * car_angle;
+        car.update_steering(input, delta_time, position_2d);
 
-        front_wheel += car.velocity.x * delta_time * car_steer;
-        back_wheel += car.velocity.x * delta_time * car_angle;
+        
+        car.velocity = car.heading * car.velocity.length();
 
-        let new_pos =(front_wheel + back_wheel) / 2.0;
+        transform.translation.x += car.velocity.x * delta_time;
+        transform.translation.y += car.velocity.y * delta_time;
 
-        transform.translation.x = new_pos.x;
-        transform.translation.y = new_pos.y;
-
-        let tan_y = front_wheel.y - back_wheel.y;
-
-        //car.heading = tan_y.atan2(front_wheel.x - back_wheel.x);
-        //car.heading = (front_wheel.x - back_wheel.x).atan2(tan_y);
-
-        //// Check for collisions with the obstacles
-        //let pos = transform.translation.xy();
-        //let rect = Rect::new(
-        //    pos.x - (car.size.x / 2.),
-        //    pos.y - (car.size.y / 2.),
-        //    pos.x + (car.size.x / 2.),
-        //    pos.y + (car.size.y / 2.),
-        //);
-        //for rectangle in col.rectangles.iter() {
-        //    let obstacle = rectangle.to_rect();
-        //    if !rect.intersect(obstacle).is_empty() {
-        //        let normal = (rect.center() - obstacle.center()).normalize();
-        //        // There's a collision, so restore the old position
-        //        let delta = normal * (obstacle.size().dot(normal) * 0.5 + 0.01);
-        //        let new_trans = old_position.xy() + delta;
-        //        transform.translation.x = new_trans.x;
-        //        transform.translation.y = new_trans.y;
-        //        //transform.translation = old_position;
-        //        break;
-        //    }
-        //}
+        transform.rotation = Quat::from_rotation_z(car.get_rotatation_rads() - (90.0_f32).to_radians());
     }
 }
